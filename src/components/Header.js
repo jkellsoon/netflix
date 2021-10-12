45a58c11e4dd63f6ce220b662d2514d02d307927
Jonathan kellson pereira de souza <familiakellson@MacBook-Air-de-Jonathan.local>
2021-10-11 22:16:42 -0300

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.css";
+
+export default ({ black }) => {
+  return (
+    <header className={black ? "black" : ""}>
+      <div className="header--logo">
+        <a href="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2880px-Netflix_2015_logo.svg.png"
+            alt="Netflix"
+          />
+        </a>
+      </div>
+      <div className="header--user">
+        <a href="/">
+          <img
+            src="https://i.pinimg.com/originals/e3/94/30/e39430434d2b8207188f880ac66c6411.png"
+            alt="Usuário"
+          />
+        </a>
+      </div>
+    </header>
+  );
+};
